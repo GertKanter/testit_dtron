21a03f93c6f300691de02fbe53cc612919d2cb90
Gert Kanter <gert.kanter@robokratt.xyz>
2018-08-22 21:56:17 +0300

    remove broekn tutorial from master branch for now

--- testit_dtron_tutorials/turtlebot_stage/scripts/insertmap.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /catkin_ws/devel/setup.bash
-
-roslaunch mongodb_store mongodb_store.launch db_path:=/opt/ros/mongodb_store & \
-sleep 10
-rosrun topological_utils insert_map.py /catkin_ws/dtronpack/generateModelFromTmap/examples/map.tmap map map
