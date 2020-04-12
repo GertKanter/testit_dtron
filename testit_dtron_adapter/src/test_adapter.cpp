bc8e77f438ca11fc3e37667553f0347e03814458
ingmar <ingmar@liibert.ee>
2020-04-12 16:56:38 +0300

    Fix

--- testit_dtron_adapter/src/test_adapter.cpp
+++ testit_dtron_adapter/src/test_adapter.cpp
@@ -84,14 +84,7 @@ void SpreadAdapter::ReaderThread() {
   SpreadMessage spreadMessage;
   do {
     spreadMessage = SpreadAdapter::ReadMessage();
-    printf(spreadMessage.Type);
-    printf("\n");
-    printf(spreadMessage.Sender);
-    printf("\n");
-    printf(spreadMessage.Group);
-    printf("\n");
-    printf(spreadMessage.Msg);
-    printf("\n");
+    std::cout << spreadMessage.Type << spreadMessage.Sender << spreadMessage.Group << spreadMessage.Msg << std::endl;
     if(spreadMessage.Type != -1) {
       callback(spreadMessage.Type, spreadMessage.Sender, spreadMessage.Group, spreadMessage.Msg);
     }
