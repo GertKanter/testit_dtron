5221c5ed23097cfa6a37f94f7ad3b11b7d72e9a2
ingmar <ingmar@liibert.ee>
2020-04-12 20:26:46 +0300

    Fix

--- testit_dtron_adapter/src/test_adapter.cpp
+++ testit_dtron_adapter/src/test_adapter.cpp
@@ -133,6 +133,7 @@ SpreadMessage SpreadAdapter::ReadMessage() {
     SP_error(ret);
     if (ret == -18) {
       std::cout << "Trying to reconnect" << std::endl;
+      SP_disconnect(MBox);
       int conn_ret = SP_connect(spreadName, userName, 0, 1, Mbox, PrivateGroup);
       if(conn_ret < 0) {
         std::cout << "Succeeded reconnect" << std::endl;
