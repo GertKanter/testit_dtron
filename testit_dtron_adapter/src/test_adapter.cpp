642e8e492af62de1f172ebcbc0227fe77ca5cf8c
ingmar <ingmar@liibert.ee>
2020-04-13 01:05:34 +0300

    Fix

--- testit_dtron_adapter/src/test_adapter.cpp
+++ testit_dtron_adapter/src/test_adapter.cpp
@@ -88,6 +88,7 @@ void SpreadAdapter::ReaderThread() {
   SpreadMessage spreadMessage;
   do {
     spreadMessage = SpreadAdapter::ReadMessage();
+    ROS_INFO("Read message")
     if (spreadMessage.Type != -1) {
       callback(spreadMessage.Type, spreadMessage.Sender, spreadMessage.Group, spreadMessage.Msg);
     }
